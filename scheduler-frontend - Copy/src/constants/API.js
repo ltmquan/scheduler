15d725c42af1f358c924be709d786781b38b983a
ltmquan <ltmquan2001@gmail.com>
2022-01-28 16:15:42 +0700

    first commit

--- scheduler-frontend - Copy/src/constants/API.js
+++
@@ -1,7 +0,0 @@
-/* API URL CONSTANTS */
-export const BASE_API_URL = "http:://localhost:8080";
-
-export const TASK_API_URL = BASE_API_URL + "/api/task";
-
-export const TASK_TYPE_API_URL = BASE_API_URL + "/api/task/type";
-/* END OF API URL CONSTANTS */
